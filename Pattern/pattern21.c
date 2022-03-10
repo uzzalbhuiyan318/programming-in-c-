128d10ec6df9af6051dc18c14c4a1606475a1c45
uzzalbhuiyan318 <21303179@iubat.edu>
2022-03-10 23:28:34 +0600

    add pattern problem

---
+++ Pattern/pattern21.c
@@ -0,0 +1,41 @@
+/*
+Take an input from user and print like the following pattern
+Sample input: 5
+Sample output:
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+0 1 0 1
+1 0 1
+0 1
+1
+Author: Uzzal_Bhuiyan
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j,space,n;
+    printf("Enter the size of pyramid: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",(i+j+1)%2);
+        }
+        printf("\n");
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",(i+j+1)%2);
+        }
+        printf("\n");
+    }
+}
+
+
+
