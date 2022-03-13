49b90a24868b4e7e9dcc920a7f2d97ce8afa26b7
uzzalbhuiyan318 <21303179@iubat.edu>
2022-03-14 00:40:47 +0600

    add another pattern

---
+++ Pattern/pattern22.c
@@ -0,0 +1,37 @@
+/*
+Take an input from user and print cross shape or X shape pattern.
+Sample input: 5
+Sample output:
+*
+* *
+*   *
+*     *
+* * * * *
+=================================================================
+Author: Uzzal_Bhuiyan
+Version: 1.0
+
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("Enter the size of row: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || j==1 || i==n)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
